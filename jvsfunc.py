05170e3bdc5b5a3707d17b10ccf0e641cd5c34da
Julek <dnjulek@hotmail.com>
2021-10-08 15:10:03 -0300

    Cosmetics

--- jvsfunc.py
+++ jvsfunc.py
@@ -6,28 +6,28 @@ from math import sqrt
 import vapoursynth as vs
 core = vs.core
 
-def expr_dilate(src, size = 2):
+def expr_dilate(src, size = 5):
     """
     Same result as core.morpho.Dilate(), faster and workable in 32 bit.
     """
     expr = _expr_matrix(size, mm = 'max')
     return core.akarin.Expr(src, expr)
 
-def expr_erode(src, size = 2):
+def expr_erode(src, size = 5):
     """
     Same result as core.morpho.Erode(), faster and workable in 32 bit.
     """
     expr = _expr_matrix(size, mm = 'min')
     return core.akarin.Expr(src, expr)
 
-def expr_close(src, size = 2):
+def expr_close(src, size = 5):
     """
     Same result as core.morpho.Close(), faster and workable in 32 bit.
     """
     close = expr_dilate(src, size)
     return expr_erode(close, size)
 
-def expr_open(src, size = 2):
+def expr_open(src, size = 5):
     """
     Same result as core.morpho.Open(), faster and workable in 32 bit.
     """
@@ -59,7 +59,7 @@ def dehalo_mask(src, expand=0.5, iterations=2, brz=255):
     mask4 = mask3 if brz < 255 else mask2
     mask4 = mask4.std.Convolution(matrix=[1, 2, 1, 2, 4, 2, 1, 2, 1])
     mask = core.std.Expr([mask1, mask4], ['x y min'])
-    return mask if is8 else depth(mask, src.format.bits_per_sample)
+    return mask if is8 else depth(mask, src.format.bits_per_sample, range=1)
 
 def FreezeFramesMod(src, mode='prev', ranges=None, single=False):
     """
@@ -140,36 +140,21 @@ def JIVTC_Deblend(src, pattern, chroma_only=True, tff=True):
         raise ValueError('JIVTC_Deblend: This filter can only be used with 60i clips.')
     
     pattern = pattern % 5
-    def deblend(src):
-        blends_a = range(1, src.num_frames - 1, 5)
-        blends_b = range(2, src.num_frames - 1, 5)
-
-        def calculate(n, f, src):
-            if n % 5 in [0, 3, 4]:
-                return src
-            else:
-                if n in blends_a:
-                    a, ab, bc, c = src[n - 1], src[n], src[n + 1], src[n + 2]
-                    dbd = core.std.Expr([a, ab, bc, c], "z a 2 / - y x 2 / - +")
-                    dbd = dbd.vinverse.Vinverse()
-                    return dbd if f.props._Combed == 1 else src
-                return src
-        
-        dbd = core.std.FrameEval(src, partial(calculate, src=src), src.tdm.IsCombed())
-        return core.std.DeleteFrames(dbd, blends_b).std.AssumeFPS(fpsnum=24000, fpsden=1001)
+    cycle10 = [[0,3,6,8], [0,2,5,8], [0,2,4,7], [2,4,6,9], [1,4,6,8]]
+    cycle08 = [[0,3,4,6], [0,2,5,6], [0,2,4,7], [0,2,4,7], [1,2,4,6]]
+    cycle05 = [[0,1,3,4], [0,1,2,4], [0,1,2,3], [1,2,3,4], [0,2,3,4]]
+    ivtced = core.std.SeparateFields(src, tff=tff).std.DoubleWeave()
+    ivtced = core.std.SelectEvery(ivtced, 10, cycle10[pattern])
 	
-    defivtc = core.std.SeparateFields(src, tff=tff).std.DoubleWeave()
-    selectlist = [[0,3,6,8], [0,2,5,8], [0,2,4,7], [2,4,6,9], [1,4,6,8]]
-    ivtced = core.std.SelectEvery(defivtc, 10, selectlist[pattern])
-	
-    selectlist = [deblend(src), ivtced[:1]+deblend(src[1:]), ivtced[:2]+deblend(src[2:]), ivtced[:2]+deblend(src[3:]), ivtced[:3]+deblend(src[4:])]
-    deblended = selectlist[pattern]
+    a, ab, bc, c = src[0] + src[:-1], src, src[1:] + src[-1], src[2:] + src[-2:]
+    deblended = core.std.Expr([a, ab, bc, c], "z a 2 / - y x 2 / - +")
+    deblended = deblended.vinverse.Vinverse()
+    deblended = core.std.SelectEvery(deblended, 5, cycle05[pattern])
 	
     inter = core.std.Interleave([ivtced, deblended])
-    selectlist = [[0,3,4,6], [0,2,5,6], [0,2,4,7], [0,2,4,7], [1,2,4,6]]
-    final = core.std.SelectEvery(inter, 8, selectlist[pattern])
+    final = core.std.SelectEvery(inter, 8, cycle08[pattern])
 	
-    final_y = core.std.ShufflePlanes([ivtced, final, final], [0, 1, 2], vs.YUV)
+    final_y = core.std.ShufflePlanes([ivtced, final], [0, 1, 2], vs.YUV)
     final = final_y if chroma_only else final
     return core.std.SetFrameProp(final, prop='_FieldBased', intval=0)
 
@@ -192,24 +177,12 @@ def jdeblend(src_fm, src, vinverse=True):
 
     a, ab, bc, c = src[0] + src[:-1], src, src[1:] + src[-1], src[2:] + src[-2:]
     dbd = core.std.Expr([a, ab, bc, c], "z a 2 / - y x 2 / - +")
+    dbd = dbd.vinverse.Vinverse() if vinverse else dbd
+
     select_src = [src.std.SelectEvery(5, i) for i in range(5)]
     select_dbd = [dbd.std.SelectEvery(5, i) for i in range(5)]
-    inter0 = core.std.Interleave([select_dbd[0], select_src[1], select_src[2], select_src[3], select_src[4]])
-    inter1 = core.std.Interleave([select_src[0], select_dbd[1], select_src[2], select_src[3], select_src[4]])
-    inter2 = core.std.Interleave([select_src[0], select_src[1], select_dbd[2], select_src[3], select_src[4]])
-    inter3 = core.std.Interleave([select_src[0], select_src[1], select_src[2], select_dbd[3], select_src[4]])
-    inter4 = core.std.Interleave([select_src[0], select_src[1], select_src[2], select_src[3], select_dbd[4]])
-
-    def calculate(n, f, src, inters):
-        comb = [f[i].props['_Combed'] for i in [0, 1]]
-        pattern = n % 5
-        if comb[0] == 1:
-            src = inters[pattern]
-        return src[n+1] if sum(comb) == 2 else src
-       
-    inters = [inter0, inter1, inter2, inter3, inter4]
-    inters = [core.vinverse.Vinverse(i) for i in inters] if vinverse else inters
-    return core.std.FrameEval(src_fm, partial(calculate, src=src_fm, inters=inters), [src_fm, src_fm[0]+src_fm[:-1]])
+    inters = _inter_pattern(select_src, select_dbd)
+    return core.std.FrameEval(src_fm, partial(_jdeblend_eval, src=src_fm, inters=inters), [src_fm, src_fm[0]+src_fm[:-1]])
 
 def jdeblend_bob(src_fm, bobbed):
     """
@@ -237,21 +210,8 @@ def jdeblend_bob(src_fm, bobbed):
 
     select_src = [src_fm.std.SelectEvery(5, i) for i in range(5)]
     select_dbd = [dbd.std.SelectEvery(5, i) for i in range(5)]
-    inter0 = core.std.Interleave([select_dbd[0], select_src[1], select_src[2], select_src[3], select_src[4]])
-    inter1 = core.std.Interleave([select_src[0], select_dbd[1], select_src[2], select_src[3], select_src[4]])
-    inter2 = core.std.Interleave([select_src[0], select_src[1], select_dbd[2], select_src[3], select_src[4]])
-    inter3 = core.std.Interleave([select_src[0], select_src[1], select_src[2], select_dbd[3], select_src[4]])
-    inter4 = core.std.Interleave([select_src[0], select_src[1], select_src[2], select_src[3], select_dbd[4]])
-
-    def calculate(n, f, src, inters):
-        comb = [f[i].props['_Combed'] for i in [0, 1]]
-        pattern = n % 5
-        if comb[0] == 1:
-            src = inters[pattern]
-        return src[n+1] if sum(comb) == 2 else src
-       
-    inters = [inter0, inter1, inter2, inter3, inter4]
-    return core.std.FrameEval(src_fm, partial(calculate, src=src_fm, inters=inters), [src_fm, src_fm[0]+src_fm[:-1]])
+    inters = _inter_pattern(select_src, select_dbd)
+    return core.std.FrameEval(src_fm, partial(_jdeblend_eval, src=src_fm, inters=inters), [src_fm, src_fm[0]+src_fm[:-1]])
 
 def jdeblend_kf(src, src_fm):
     """
@@ -282,7 +242,7 @@ def find_comb(src, name='comb_list'):
     """
     frames = []
     src = depth(src, 8) if src.format.bits_per_sample != 8 else src
-    find = core.tdm.IsCombed(src).std.PlaneStats()
+    find = core.tdm.IsCombed(src)
 
     def _cb(n, f):
         if get_prop(f, "_Combed", int) == 1:
@@ -390,3 +350,18 @@ def _expr_matrix(size = 2, mm = 'max'):
     matrix = even1 + even2 if is_even else odd
     matrix = ''.join(matrix)
     return matrix[:8] + matrix[12:]
+
+def _inter_pattern(clipa, clipb):
+    inter0 = core.std.Interleave([clipb[0], clipa[1], clipa[2], clipa[3], clipa[4]])
+    inter1 = core.std.Interleave([clipa[0], clipb[1], clipa[2], clipa[3], clipa[4]])
+    inter2 = core.std.Interleave([clipa[0], clipa[1], clipb[2], clipa[3], clipa[4]])
+    inter3 = core.std.Interleave([clipa[0], clipa[1], clipa[2], clipb[3], clipa[4]])
+    inter4 = core.std.Interleave([clipa[0], clipa[1], clipa[2], clipa[3], clipb[4]])
+    return [inter0, inter1, inter2, inter3, inter4]
+
+def _jdeblend_eval(n, f, src, inters):
+    comb = [f[i].props['_Combed'] for i in [0, 1]]
+    pattern = n % 5
+    if comb[0] == 1:
+        src = inters[pattern]
+    return src[n+1] if sum(comb) == 2 else src
