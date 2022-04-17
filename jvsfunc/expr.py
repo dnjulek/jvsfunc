7c9e6db89d110264d4f53d9c5507914e77e2f164
Julek <dnjulek@hotmail.com>
2022-04-17 19:19:13 -0300

    Add modes for ccd

--- jvsfunc/expr.py
+++ jvsfunc/expr.py
@@ -2,58 +2,133 @@
 Some functions implemented with core.akarin.Expr
 """
 
+from vsutil import plane, depth, get_depth, split, join
 from .util import morpho_matrix, ex_matrix
-from vsutil import plane, depth, get_depth
+from lvsfunc.scale import ssim_downsample
+from lvsfunc.util import get_prop
+from typing import Any
 import vapoursynth as vs
 core = vs.core
 
 
-def ccd(src: vs.VideoNode, threshold: float = 4) -> vs.VideoNode:
+def ccd(src: vs.VideoNode,
+        threshold: float = 4,
+        mode: int = 1,
+        matrix: int | None = None,
+        **kwargs: Any) -> vs.VideoNode:
     """
     Yet another Camcorder Color Denoise implementation.
+
+    :param src: RGBS or YUV input clip (converted to RGBS when YUV).
+    :param threshold: Chroma denoise threshold.
+    :param mode: How the YUV -> RGB -> YUV conversion is done:
+                 mode=1: bicubic to chroma resolution, and then back to YUV with bicubic.
+                 mode=2: bicubic to luma resolution, and then back to YUV with bicubic.
+                 mode=3: nnedi3 to luma resolution, and then back to YUV with bicubic.
+                 mode=4: nnedi3 to luma resolution, and then back to YUV with ssim_downsample.
+    :param matrix: YUV matrix coefficient. If None, 1 will be used for HD and 6 for SD.
+    :param kwargs: Arguments passed to ssim_downsample.
     """
 
-    if src.format.color_family != vs.RGB or src.format.sample_type != vs.FLOAT:
-        raise ValueError('ccd: only RGBS format is supported, use jvsfunc.ccdmod() for YUV.')
+    if src.format.color_family == vs.GRAY:
+        raise ValueError('ccd: GRAY format is not supported.')
+
+    if mode not in [1, 2, 3, 4]:
+        raise ValueError('ccd: Not supported mode, use 1, 2, 3 or 4.')
 
     thr = threshold**2/195075.0
-    r = core.std.ShufflePlanes([src, src, src], [0, 0, 0], vs.RGB)
-    g = core.std.ShufflePlanes([src, src, src], [1, 1, 1], vs.RGB)
-    b = core.std.ShufflePlanes([src, src, src], [2, 2, 2], vs.RGB)
 
-    ex_ccd = core.akarin.Expr([r, g, b, src],
-                              'x[-12,-12] x - 2 pow y[-12,-12] y - 2 pow z[-12,-12] z - 2 pow + + A! '
-                              'x[-4,-12] x - 2 pow y[-4,-12] y - 2 pow z[-4,-12] z - 2 pow + + B! '
-                              'x[4,-12] x - 2 pow y[4,-12] y - 2 pow z[4,-12] z - 2 pow + + C! '
-                              'x[12,-12] x - 2 pow y[12,-12] y - 2 pow z[12,-12] z - 2 pow + + D! '
-                              'x[-12,-4] x - 2 pow y[-12,-4] y - 2 pow z[-12,-4] z - 2 pow + + E! '
-                              'x[-4,-4] x - 2 pow y[-4,-4] y - 2 pow z[-4,-4] z - 2 pow + + F! '
-                              'x[4,-4] x - 2 pow y[4,-4] y - 2 pow z[4,-4] z - 2 pow + + G! '
-                              'x[12,-4] x - 2 pow y[12,-4] y - 2 pow z[12,-4] z - 2 pow + + H! '
-                              'x[-12,4] x - 2 pow y[-12,4] y - 2 pow z[-12,4] z - 2 pow + + I! '
-                              'x[-4,4] x - 2 pow y[-4,4] y - 2 pow z[-4,4] z - 2 pow + + J! '
-                              'x[4,4] x - 2 pow y[4,4] y - 2 pow z[4,4] z - 2 pow + + K! '
-                              'x[12,4] x - 2 pow y[12,4] y - 2 pow z[12,4] z - 2 pow + + L! '
-                              'x[-12,12] x - 2 pow y[-12,12] y - 2 pow z[-12,12] z - 2 pow + + M! '
-                              'x[-4,12] x - 2 pow y[-4,12] y - 2 pow z[-4,12] z - 2 pow + + N! '
-                              'x[4,12] x - 2 pow y[4,12] y - 2 pow z[4,12] z - 2 pow + + O! '
-                              'x[12,12] x - 2 pow y[12,12] y - 2 pow z[12,12] z - 2 pow + + P! '
-                              f'A@ {thr} < 1 0 ? B@ {thr} < 1 0 ? C@ {thr} < 1 0 ? D@ {thr} < 1 0 ? '
-                              f'E@ {thr} < 1 0 ? F@ {thr} < 1 0 ? G@ {thr} < 1 0 ? H@ {thr} < 1 0 ? '
-                              f'I@ {thr} < 1 0 ? J@ {thr} < 1 0 ? K@ {thr} < 1 0 ? L@ {thr} < 1 0 ? '
-                              f'M@ {thr} < 1 0 ? N@ {thr} < 1 0 ? O@ {thr} < 1 0 ? P@ {thr} < 1 0 ? '
-                              '+ + + + + + + + + + + + + + + 1 + Q! '
-                              f'A@ {thr} < a[-12,-12] 0 ? B@ {thr} < a[-4,-12] 0 ? '
-                              f'C@ {thr} < a[4,-12] 0 ? D@ {thr} < a[12,-12] 0 ? '
-                              f'E@ {thr} < a[-12,-4] 0 ? F@ {thr} < a[-4,-4] 0 ? '
-                              f'G@ {thr} < a[4,-4] 0 ? H@ {thr} < a[12,-4] 0 ? '
-                              f'I@ {thr} < a[-12,4] 0 ? J@ {thr} < a[-4,4] 0 ? '
-                              f'K@ {thr} < a[4,4] 0 ? L@ {thr} < a[12,4] 0 ? '
-                              f'M@ {thr} < a[-12,12] 0 ? N@ {thr} < a[-4,12] 0 ? '
-                              f'O@ {thr} < a[4,12] 0 ? P@ {thr} < a[12,12] 0 ? '
-                              '+ + + + + + + + + + + + + + + a + Q@ /')
+    def nnup(src: vs.VideoNode) -> vs.VideoNode:
+        src = src.nnedi3.nnedi3(1, 1, 0, 0, 3, 2).std.Transpose()
+        return src.nnedi3.nnedi3(1, 1, 0, 0, 3, 2).std.Transpose().resize.Bicubic(src_top=-0.5)
 
-    return ex_ccd
+    def nnrgb(src: vs.VideoNode, matrix) -> vs.VideoNode:
+        yuv = split(src)
+        u, v = nnup(yuv[1]), nnup(yuv[2])
+        return join([yuv[0], u, v]).resize.Point(format=vs.RGBS, matrix_in=matrix)
+
+    def get_matrix(src: vs.VideoNode) -> int:
+        frame = src.get_frame(0)
+        guess = 1 if src.width > 1270 or src.height > 710 else 6
+
+        if "_Matrix" in frame.props:
+            matrix = get_prop(frame, "_Matrix", int)
+        else:
+            matrix = guess
+        return guess if matrix == 2 else matrix
+
+    def expr(src: vs.VideoNode) -> vs.VideoNode:
+        r = core.std.ShufflePlanes([src, src, src], [0, 0, 0], vs.RGB)
+        g = core.std.ShufflePlanes([src, src, src], [1, 1, 1], vs.RGB)
+        b = core.std.ShufflePlanes([src, src, src], [2, 2, 2], vs.RGB)
+
+        ex_ccd = core.akarin.Expr([r, g, b, src],
+                                  'x[-12,-12] x - 2 pow y[-12,-12] y - 2 pow z[-12,-12] z - 2 pow + + A! '
+                                  'x[-4,-12] x - 2 pow y[-4,-12] y - 2 pow z[-4,-12] z - 2 pow + + B! '
+                                  'x[4,-12] x - 2 pow y[4,-12] y - 2 pow z[4,-12] z - 2 pow + + C! '
+                                  'x[12,-12] x - 2 pow y[12,-12] y - 2 pow z[12,-12] z - 2 pow + + D! '
+                                  'x[-12,-4] x - 2 pow y[-12,-4] y - 2 pow z[-12,-4] z - 2 pow + + E! '
+                                  'x[-4,-4] x - 2 pow y[-4,-4] y - 2 pow z[-4,-4] z - 2 pow + + F! '
+                                  'x[4,-4] x - 2 pow y[4,-4] y - 2 pow z[4,-4] z - 2 pow + + G! '
+                                  'x[12,-4] x - 2 pow y[12,-4] y - 2 pow z[12,-4] z - 2 pow + + H! '
+                                  'x[-12,4] x - 2 pow y[-12,4] y - 2 pow z[-12,4] z - 2 pow + + I! '
+                                  'x[-4,4] x - 2 pow y[-4,4] y - 2 pow z[-4,4] z - 2 pow + + J! '
+                                  'x[4,4] x - 2 pow y[4,4] y - 2 pow z[4,4] z - 2 pow + + K! '
+                                  'x[12,4] x - 2 pow y[12,4] y - 2 pow z[12,4] z - 2 pow + + L! '
+                                  'x[-12,12] x - 2 pow y[-12,12] y - 2 pow z[-12,12] z - 2 pow + + M! '
+                                  'x[-4,12] x - 2 pow y[-4,12] y - 2 pow z[-4,12] z - 2 pow + + N! '
+                                  'x[4,12] x - 2 pow y[4,12] y - 2 pow z[4,12] z - 2 pow + + O! '
+                                  'x[12,12] x - 2 pow y[12,12] y - 2 pow z[12,12] z - 2 pow + + P! '
+                                  f'A@ {thr} < 1 0 ? B@ {thr} < 1 0 ? C@ {thr} < 1 0 ? D@ {thr} < 1 0 ? '
+                                  f'E@ {thr} < 1 0 ? F@ {thr} < 1 0 ? G@ {thr} < 1 0 ? H@ {thr} < 1 0 ? '
+                                  f'I@ {thr} < 1 0 ? J@ {thr} < 1 0 ? K@ {thr} < 1 0 ? L@ {thr} < 1 0 ? '
+                                  f'M@ {thr} < 1 0 ? N@ {thr} < 1 0 ? O@ {thr} < 1 0 ? P@ {thr} < 1 0 ? '
+                                  '+ + + + + + + + + + + + + + + 1 + Q! '
+                                  f'A@ {thr} < a[-12,-12] 0 ? B@ {thr} < a[-4,-12] 0 ? '
+                                  f'C@ {thr} < a[4,-12] 0 ? D@ {thr} < a[12,-12] 0 ? '
+                                  f'E@ {thr} < a[-12,-4] 0 ? F@ {thr} < a[-4,-4] 0 ? '
+                                  f'G@ {thr} < a[4,-4] 0 ? H@ {thr} < a[12,-4] 0 ? '
+                                  f'I@ {thr} < a[-12,4] 0 ? J@ {thr} < a[-4,4] 0 ? '
+                                  f'K@ {thr} < a[4,4] 0 ? L@ {thr} < a[12,4] 0 ? '
+                                  f'M@ {thr} < a[-12,12] 0 ? N@ {thr} < a[-4,12] 0 ? '
+                                  f'O@ {thr} < a[4,12] 0 ? P@ {thr} < a[12,12] 0 ? '
+                                  '+ + + + + + + + + + + + + + + a + Q@ /')
+        return ex_ccd
+
+    if src.format.color_family == vs.YUV:
+
+        if matrix is None:
+            matrix = get_matrix(src)
+
+        if src.format.subsampling_h and src.format.subsampling_w:
+            src444 = src.format.replace(subsampling_w=0, subsampling_h=0).id
+            cw = src.width//2
+            ch = src.height//2
+
+            if mode == 1:
+                rgbs = src.resize.Bicubic(cw, ch, format=vs.RGBS, matrix_in=matrix)
+                den = expr(rgbs).resize.Bicubic(format=src444, matrix=matrix, src_left=-0.25)
+            elif mode == 2:
+                rgbs = src.resize.Bicubic(format=vs.RGBS, matrix_in=matrix)
+                den = expr(rgbs).resize.Bicubic(format=src.format.id, matrix=matrix)
+            elif mode == 3:
+                rgbs = nnrgb(src, matrix)
+                den = expr(rgbs).resize.Bicubic(format=src.format.id, matrix=matrix)
+            else:
+                rgbs = nnrgb(src, matrix)
+                den = expr(rgbs).resize.Bicubic(format=vs.YUV444PS, matrix=matrix, src_left=-0.5)
+                u = ssim_downsample(plane(den, 1), cw, ch, **kwargs)
+                v = ssim_downsample(plane(den, 2), cw, ch, **kwargs)
+                den = core.std.ShufflePlanes([den, u, v], [0, 0, 0], vs.YUV)
+                den = den.resize.Point(format=src.format.id)
+        else:
+            rgbs = src.resize.Bicubic(format=vs.RGBS, matrix_in=matrix)
+            den = expr(rgbs).resize.Bicubic(format=src.format.id, matrix=matrix)
+        den = core.std.ShufflePlanes([src, den], [0, 1, 2], vs.YUV)
+    else:
+        rgbs = depth(src, 32)
+        den = expr(rgbs)
+    return den
 
 
 def ccdmod(src: vs.VideoNode, threshold: float = 4, matrix: int | None = None) -> vs.VideoNode:
