29a317be907eb127d8c359faf877e97d4d6f987e
Julek <dnjulek@hotmail.com>
2022-04-24 13:57:27 -0300

    Add find_prop

--- jvsfunc/find.py
+++ jvsfunc/find.py
@@ -3,11 +3,57 @@ Some functions implemented with core.akarin.Expr
 """
 
 from lvsfunc.render import clip_async_render
-from lvsfunc.util import get_prop
 from .util import bookmarks, rng
 from vsutil import depth
+from typing import Type
+import operator as opr
 import vapoursynth as vs
 core = vs.core
+
+
+def find_prop(src: vs.VideoNode,
+              prop: str,
+              operator: str,
+              ref: int | float,
+              name: str = "find_prop",
+              return_ranges: bool = False,
+              min_length: int = 24,
+              ) -> vs.VideoNode:
+    """
+    Creates a VSEdit Bookmarks file with a list of frames, based on the given ``prop``, ``operator`` and ``ref``.
+    Check find_comb, find_30p or find_60p for examples.
+
+    :param src: Input clip.
+    :param prop: Frame prop to be used.
+    :param operator: Conditional operator to apply between prop and ref ("<", "<=", "==", "!=", ">" or ">=").
+    :param ref: Value to be compared with prop.
+    :param name: Output file name.
+    :param return_ranges: Return only the first and last frame of each sequence.
+    :param min_length: Amount of frames to finish a sequence, to avoid false negatives.
+    """
+
+    ops = {
+        "<": opr.lt,
+        "<=": opr.le,
+        "==": opr.eq,
+        "!=": opr.ne,
+        ">": opr.gt,
+        ">=": opr.ge,
+    }
+
+    frames = []
+    dbug = "Searching "+prop+" "+operator+" "+str(ref)+"..."
+
+    def _cb(n: int, f: vs.VideoFrame):
+        fprop = f.props[prop]
+        if ops[operator](fprop, ref):  # type: ignore
+            frames.append(n)
+
+    clip_async_render(src, progress=dbug, callback=_cb)
+    if return_ranges:
+        frames = rng(frames, min_length)
+    frames = bookmarks(frames, name)
+    return src
 
 
 def find_comb(src: vs.VideoNode, name: str = 'comb_list') -> vs.VideoNode:
@@ -17,21 +63,17 @@ def find_comb(src: vs.VideoNode, name: str = 'comb_list') -> vs.VideoNode:
     :param src: Input clip.
     :param name: Output file name.
     """
-    frames = []
     src = depth(src, 8)
     find = core.tdm.IsCombed(src)
-
-    def _cb(n, f):
-        if get_prop(f, "_Combed", int) == 1:
-            frames.append(n)
-
-    clip_async_render(find, progress="Searching combed...", callback=_cb)
-    frames = bookmarks(frames, name)
-    return find
+    return find_prop(find, prop="_Combed", operator="==", ref=1, name=name)
 
 
-def find_30p(src: vs.VideoNode, min_length: int = 34, thr: int = 2000, name: str = '30p_ranges',
-             show_thr: bool = False) -> vs.VideoNode:
+def find_30p(src: vs.VideoNode,
+             min_length: int = 34,
+             thr: int = 2000,
+             name: str = '30p_ranges',
+             show_thr: bool = False
+             ) -> vs.VideoNode:
     """
     Creates a VSEdit Bookmarks file with possible 30 fps ranges from a VFR (interlaced) clip,
     to be used in scene filtering.
@@ -45,18 +87,12 @@ def find_30p(src: vs.VideoNode, min_length: int = 34, thr: int = 2000, name: str
     if src.fps_num != 30000 or src.fps_den != 1001:
         raise ValueError('find_30p: This script can only be used with 60i clips.')
 
-    frames = []
     find = core.vivtc.VFM(src, order=1, mode=0).vivtc.VDecimate(dryrun=True)
     if show_thr:
         return core.text.FrameProps(find, "VDecimateMaxBlockDiff")
 
-    def _cb(n, f):
-        if get_prop(f, "VDecimateMaxBlockDiff", int) > thr:
-            frames.append(n)
-
-    clip_async_render(find, progress="Searching 30 fps ranges...", callback=_cb)
-    frames = rng(frames, min_length)
-    frames = bookmarks(frames, name)
+    find = find_prop(find, prop="VDecimateMaxBlockDiff", operator=">", ref=thr,
+                     name=name, return_ranges=True, min_length=min_length)
     return find
 
 
@@ -72,14 +108,7 @@ def find_60p(src: vs.VideoNode, min_length: int = 60, name: str = '60p_ranges') 
     if src.fps_num != 30000 or src.fps_den != 1001:
         raise ValueError('find_60p: This script can only be used with 60i clips.')
 
-    frames = []
     find = core.vivtc.VFM(src, order=1, mode=2)
-
-    def _cb(n, f):
-        if get_prop(f, "_Combed", int) == 0:
-            frames.append(n)
-
-    clip_async_render(find, progress="Searching 60 fps ranges...", callback=_cb)
-    frames = rng(frames, min_length)
-    frames = bookmarks(frames, name)
+    find = find_prop(find, prop="_Combed", operator="==", ref=0, name=name,
+                     return_ranges=True, min_length=min_length)
     return find
