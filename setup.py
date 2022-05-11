ef910ec7d499f20df11fa99e8bf4e60401abf36e
Julek <dnjulek@hotmail.com>
2022-05-11 00:46:38 -0300

    bump ver

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='jvsfunc',
-    version='1.0.0',
+    version='1.0.1',
     description='Julek VapourSynth functions',
     url='https://github.com/dnjulek/jvsfunc',
     license='MIT',
