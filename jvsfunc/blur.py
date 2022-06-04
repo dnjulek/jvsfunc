5d6c6df7536d4807289f59d72ef231ac118a3a0f
Julek <dnjulek@hotmail.com>
2022-06-04 15:50:46 -0300

    add sbr

--- jvsfunc/blur.py
+++ jvsfunc/blur.py
@@ -2,9 +2,58 @@
 Blur functions
 """
 
+from typing import Sequence
 from .util import ex_matrix
+from vsutil import get_neutral_value
 import vapoursynth as vs
 core = vs.core
+
+
+def sbr(src: vs.VideoNode, r: int = 1, mode: str = 'hv', planes: int | Sequence[int] | None = None) -> vs.VideoNode:
+    """
+    A faster sbr implementation, using Convolution only once even for r 2 and 3.
+    A single function for all modes (sbr, sbrH and sbrV).
+
+    :param src: Input clip.
+    :param r: 1, 2 or 3 (blur strength).
+    :param mode: 'h', 'v' or 'hv' (std.Convolution modes).
+    :param planes: Planes to process.
+    """
+
+    neutral = get_neutral_value(src, chroma=True)
+    plane_range = range(src.format.num_planes)
+
+    if planes is None:
+        planes = list(plane_range)
+    elif isinstance(planes, int):
+        planes = [planes]
+
+    match mode:
+        case 'hv':
+            matrix2 = [1, 3, 4, 3, 1]
+            matrix3 = [1, 4, 8, 10, 8, 4, 1]
+        case 'h' | 'v':
+            matrix2 = [1, 6, 15, 20, 15, 6, 1]
+            matrix3 = [1, 10, 45, 120, 210, 252, 210, 120, 45, 10, 1]
+        case _:
+            raise ValueError("sbr: Invalid mode, use 'h', 'v' or 'hv'.")
+
+    match r:
+        case 1:
+            matrix = [1, 2, 1]
+        case 2:
+            matrix = matrix2
+        case 3:
+            matrix = matrix3
+        case _:
+            raise ValueError("sbr: Invalid r, use 1, 2 or 3.")
+
+    expr = f'x y - x {neutral} - * 0 < {neutral} x y - abs x {neutral} - abs < x y - {neutral} + x ? ?'
+    rg11 = src.std.Convolution(matrix=matrix, planes=planes, mode=mode)
+    rg11d = core.std.MakeDiff(src, rg11, planes=planes)
+    rg11ds = rg11d.std.Convolution(matrix=matrix, planes=planes, mode=mode)
+    rg11dd = core.std.Expr([rg11d, rg11ds], [expr if i in planes else '' for i in plane_range])
+    return core.std.MakeDiff(src, rg11dd, planes=planes)
 
 
 def medianblur(src: vs.VideoNode, radius: int = 2) -> vs.VideoNode:
