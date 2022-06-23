a90dfa2e85de03e694c708d012f36783aeb44053
Julek <dnjulek@hotmail.com>
2022-06-22 23:53:23 -0300

    add retinex_edgemask

--- jvsfunc/mask.py
+++ jvsfunc/mask.py
@@ -4,10 +4,33 @@ Masks functions
 
 from __future__ import annotations
 
-from vsutil import depth, iterate, get_depth, get_y
+from vsutil import depth, iterate, get_depth, get_y, get_peak_value, scale_value
+from .misc import retinex
 from math import sqrt
 import vapoursynth as vs
 core = vs.core
+
+
+def retinex_edgemask(src: vs.VideoNode, sigma=1, brz: int = 8000) -> vs.VideoNode:
+    """
+    retinex_edgemask from kagefunc using jvsfunc.retinex rather than the plugin.
+
+    :param src: Input clip.
+    :param sigma: sigma of tcanny.
+    :param brz: if brz > 0 output will be binarized (brz uses 16-bit scale).
+    """
+    luma = get_y(src)
+    max_value = get_peak_value(src)
+    sbrz = scale_value(brz, 16, get_depth(src))
+    ret = retinex(luma, [50, 200, 350])
+    tcanny = ret.tcanny.TCanny(mode=1, sigma=sigma).std.Minimum(coordinates=[1, 0, 1, 0, 0, 1, 0, 1])
+    kirsch1 = luma.std.Convolution(matrix=[5,  5,  5, -3,  0, -3, -3, -3, -3], saturate=False)
+    kirsch2 = luma.std.Convolution(matrix=[-3,  5,  5, -3,  0,  5, -3, -3, -3], saturate=False)
+    kirsch3 = luma.std.Convolution(matrix=[-3, -3,  5, -3,  0,  5, -3, -3,  5], saturate=False)
+    kirsch4 = luma.std.Convolution(matrix=[-3, -3, -3, -3,  0,  5, -3,  5,  5], saturate=False)
+    expr = f'x y z a max max max b + {max_value} min'
+    expr_brz = f'x y z a max max max b + {sbrz} > {max_value} 0 ?'
+    return core.akarin.Expr([kirsch1, kirsch2, kirsch3, kirsch4, tcanny], expr_brz if brz > 0 else expr)
 
 
 def dehalo_mask(src: vs.VideoNode, expand: float = 0.5, iterations: int = 2, brz: int = 255) -> vs.VideoNode:
