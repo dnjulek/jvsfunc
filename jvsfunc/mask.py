9c3daac8f641edadbf9b8ad4c8632d090a527cd0
Julek <dnjulek@hotmail.com>
2022-06-23 00:14:36 -0300

    update retinex_edgemask

--- jvsfunc/mask.py
+++ jvsfunc/mask.py
@@ -5,25 +5,30 @@ Masks functions
 from __future__ import annotations
 
 from vsutil import depth, iterate, get_depth, get_y, get_peak_value, scale_value
+from typing import Sequence
 from .misc import retinex
 from math import sqrt
 import vapoursynth as vs
 core = vs.core
 
 
-def retinex_edgemask(src: vs.VideoNode, sigma=1, brz: int = 8000) -> vs.VideoNode:
+def retinex_edgemask(src: vs.VideoNode,
+                     tcanny_sigma: float | int = 1,
+                     retinex_sigmas: Sequence[float | int] = [50, 200, 350],
+                     brz: int = 8000) -> vs.VideoNode:
     """
     retinex_edgemask from kagefunc using jvsfunc.retinex rather than the plugin.
 
     :param src: Input clip.
-    :param sigma: sigma of tcanny.
+    :param tcanny_sigma: sigma of tcanny.
+    :param retinex_sigmas: sigma list of retinex.
     :param brz: if brz > 0 output will be binarized (brz uses 16-bit scale).
     """
     luma = get_y(src)
     max_value = get_peak_value(src)
     sbrz = scale_value(brz, 16, get_depth(src))
-    ret = retinex(luma, [50, 200, 350])
-    tcanny = ret.tcanny.TCanny(mode=1, sigma=sigma).std.Minimum(coordinates=[1, 0, 1, 0, 0, 1, 0, 1])
+    ret = retinex(luma, retinex_sigmas)
+    tcanny = ret.tcanny.TCanny(mode=1, sigma=tcanny_sigma).std.Minimum(coordinates=[1, 0, 1, 0, 0, 1, 0, 1])
     kirsch1 = luma.std.Convolution(matrix=[5,  5,  5, -3,  0, -3, -3, -3, -3], saturate=False)
     kirsch2 = luma.std.Convolution(matrix=[-3,  5,  5, -3,  0,  5, -3, -3, -3], saturate=False)
     kirsch3 = luma.std.Convolution(matrix=[-3, -3,  5, -3,  0,  5, -3, -3,  5], saturate=False)
