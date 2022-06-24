a9c5588e64560617e379689e65302b807a6b019c
Julek <dnjulek@hotmail.com>
2022-06-24 12:41:05 -0300

    bump ver

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as fh:
 
 setup(
     name='jvsfunc',
-    version='1.0.2',
+    version='1.0.3',
     description='Julek VapourSynth functions',
     long_description=long_desc,
     long_description_content_type="text/markdown",
